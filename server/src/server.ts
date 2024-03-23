b5a7b69b42ee906ee9e546c667dd4714bda920e9
Manish <doluibimal908@gmail.com>
2024-03-23 10:35:23 +0530

    socket.io integration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,49 @@
-import dotenv from 'dotenv';
-import { app } from './app';
+import http from 'http'
+import express from 'express'
+import {Server} from 'socket.io'
 
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
-app.listen(3000, () => {
-  console.log("server listening on port: 3000");
+import cors from 'cors'
+
+
+const app = express();
+const server =  http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }
+});
+
+const PORT = process.env.PORT || 8000;
+
+//middleware
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}))
+
+
+app.get('/', (req, res) => {
+  res.send("server is working...")
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  console.log(`id: ${socket.id}`);
   
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg)    
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 })
